80e0e031836dce8e0464120561dd19126f90c96a
alex <alexgalayda@gmail.com>
2026-07-01 12:51:30 +0000

    hot fix

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 import os
 from app import create_app
+from app.extensions import db
 
 app = create_app(os.environ.get('FLASK_ENV', 'development'))
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run()
